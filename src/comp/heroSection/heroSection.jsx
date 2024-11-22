46189df5155bef5664aa7fcbf430a84f419cab3a
Hiten Vadhaiya <hiten.v@trioency.com>
2024-11-22 16:35:23 +0530

    Update heroSection.jsx

--- src/comp/heroSection/heroSection.jsx
+++ src/comp/heroSection/heroSection.jsx
@@ -40,7 +40,7 @@ export default function HeroSection() {
               <Bars3Icon aria-hidden="true" className="size-6" />
             </button> */}
           </div>
-          <div className="flex lg:flex-1 lg:gap-x-12">
+          <div className="flex lg:gap-x-12">
             {/* {navigation.map((item) => (
               <a key={item.name} href={item.href} className="text-sm/6 font-semibold text-gray-900">
                 {item.name}
